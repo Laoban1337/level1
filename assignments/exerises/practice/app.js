f6c10df5823b29bc6cb0e1908c2fec4fcef47252
Brian Monestime <bmonestime@gmail.com>
2024-01-07 14:32:39 -0700

    Added more to the practice project

--- assignments/exerises/practice/app.js
+++ assignments/exerises/practice/app.js
@@ -1,10 +1,40 @@
-var result = document.querySelectorAll(".favorite-things>li");
-var result1 = document.getElementById("rude");
-var element = document.getElementsByClassName("fish")
- 
-element.style.color = "white";
-result.forEach((result) => {
-  result.textContent = " I won't tell";
-});
+// var result = document.querySelectorAll(".rarity  > li");
 
-result1.textContent = "Very rude people";
+var edgemireLocation = document.querySelector(".edge");
+
+var legendText = document.querySelector(".legend");
+var epicText = document.querySelector(".epic");
+var pristineText = document.querySelector(".pristine");
+var rareText = document.querySelector(".rare");
+var uncommonText = document.querySelector(".uncommon");
+var normalText = document.querySelector(".normal");
+
+document.body.style.backgroundColor = "black";
+
+legendText.style.color = "#FF004D";
+epicText.style.color = "#FAEF9B";
+pristineText.style.color = "#80BCBD";
+rareText.style.color = "#B15EFF";
+uncommonText.style.color = "#65B741";
+normalText.style.color = "#FBF6EE";
+
+edgemireLocation.textContent = "Extremely rare location";
+
+var powerRangers = [
+  { name: "Jason Lee Scott", color: "Red", weapon: "Power Sword" },
+  { name: "Kimberly Hart", color: "Pink", weapon: "Power Bow" },
+  { name: "Zack Taylor", color: "Black", weapon: "Power Axe" },
+  { name: "Trini Kwan", color: "Yellow", weapon: "Power Daggers" },
+  { name: "Billy Cranston", color: "Blue", weapon: "Power Lance" },
+  { name: "Tommy Oliver", color: "Green/White", weapon: "Dragon Dagger/Saba" }
+];
+
+var rangersList = document.getElementById("rangers");
+
+for(var i = 0; i < powerRangers.length; i++){
+  var ranger = powerRangers[i];
+  var listItem = document.createElement("li")
+
+  listItem.textContent = "Name : " + " " + ranger.name + " Suit color: " +  ranger.color  + "  weapon of choice  " + ranger.weapon;
+  rangersList.appendChild(listItem);
+}

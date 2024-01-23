cac9bff7242e1e406eae04f452abb5e0adde7a31
Brian Monestime <bmonestime@gmail.com>
2024-01-23 16:58:29 -0700

    Problems Solved

---
+++ assignments/exerises/problemSolving/main.js
@@ -0,0 +1,54 @@
+
+/*
+Write a function that takes an array of numbers and returns the largest (without using Math.max())
+*/
+// const nums = [1500, 8, 654, 205]
+// const nums1 = [3850, 205, 65, 147, 612, 900];
+// const nums2 = [10000, 2050, 350, 47, 12, 9200];
+// let maxVal = -Infinity;
+
+// function largest(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     let element = array[i];
+//     if (element > maxVal) {
+//       maxVal = element;
+//       console.log(maxVal)
+//     } 
+
+//   }
+// }
+
+// largest(nums);
+// largest(nums1);
+// largest(nums2);
+
+
+/*
+Write a function that takes an array of words and a character and returns each word that has that character present.
+*/
+//  const words =["Ginger!","@#$%", "World", "apple"]
+// function compare(array, char){
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         if (element.includes(char)) {
+//             console.log(element)
+//         }
+        
+//     }
+// }
+// compare(words,"a");
+
+
+/*
+   Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2
+*/
+
+// function isDivisible(num1, num2){
+//     if ((num1 % num2 )=== 0) {
+//         console.log("true")
+//     }else{
+//         console.log("false")
+//     }
+// }
+
+// isDivisible(15,2);

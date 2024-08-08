1262352f7a9589cf2ece73a97a9e52d5ab5d495c
Brian Monestime <bmonestime@gmail.com>
2024-08-07 20:06:55 -0600

    final commit before deployment

--- assignments/htmlCalculator/app.js
+++ assignments/htmlCalculator/app.js
@@ -1,4 +1,6 @@
-const form = document["my-form"];
+const form = document.myForm
+const subForm = document.myForm1
+const multiplyForm = document.myForm2
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -8,38 +10,50 @@ form.addEventListener("submit", function (event) {
 
   const num2 = form.num2.value;
   form.num2.value = "";
-  const operation = form.operation.value;
-  if (operation === "add") {
-    add(num1, num2);
-  }
-   else if (operation === "sub") {
-    sub(num1, num2);
-  }
-  else if (operation === "multiply") {
-    multiply(num1, num2);
-  }
+  add(num1, num2)
+  
 });
 
+subForm.addEventListener("submit", function(event){
+  event.preventDefault();
+  const num1 = subForm.num1.value;
+  form.num1.value = "";
+
+  const num2 = subForm.num2.value;
+  form.num2.value = "";
+  sub(num1, num2)
+})
+
+multiplyForm.addEventListener("submit", function(event){
+  event.preventDefault();
+  const num1 = multiplyForm.num1.value;
+  form.num1.value = "";
+
+  const num2 = multiplyForm.num2.value;
+  form.num2.value = "";
+  multiply(num1, num2)
+})
+
 function add(num1, num2) {
-  const result = " the result of your calculation is :";
+  const result1 = " the result of your add calculation is :";
   const h3 = document.createElement("h3");
   h3.textContent = +num1 + +num2;
-  document.getElementsByTagName("body")[0].append(result);
+  document.getElementsByTagName("body")[0].append(result1);
   document.getElementsByTagName("body")[0].append(h3);
 }
 
 function sub(num1, num2) {
-  const result = " the result of your calculation is :";
+  const result2 = " the result of your subtraction calculation is :";
   const h3 = document.createElement("h3");
   h3.textContent = +num1 - +num2;
-  document.getElementsByTagName("body")[0].append(result);
+  document.getElementsByTagName("body")[0].append(result2);
   document.getElementsByTagName("body")[0].append(h3);
 }
 
 function multiply(num1, num2) {
-  const result = " the result of your calculation is :";
-  const h3 = document.createElement("h3");
-  h3.textContent = +num1 * +num2;
-  document.getElementsByTagName("body")[0].append(result);
+  const result3 = " the result of your multiplication calculation is :";
+    h3 = document.createElement("h2");
+  h3.textContent = num1 * num2;
+  document.getElementsByTagName("body")[0].append(result3);
   document.getElementsByTagName("body")[0].append(h3);
 }
